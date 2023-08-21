58a4a20b749a9f1d699992441532b37256409d44
Andrés <andrew.seacord@gmail.com>
2023-08-20 21:41:28 -0400

    WIP

--- src/network.rs
+++ src/network.rs
@@ -3,10 +3,10 @@ use crate::{layer::Layer, neuron::Neuron, weight::Weight, input_neuron::InputNeu
 #[derive(Clone, Debug, Default, PartialEq, PartialOrd)]
 /// A network object. `layers` refers to non-input layers.
 pub struct Network {
-    input: Vec<InputNeuron>,
-    layers: Vec<Layer>,
-    neurons: Vec<Neuron>,
-    weights: Vec<Weight>,
+    pub(crate) input: Vec<InputNeuron>,
+    pub(crate) layers: Vec<Layer>,
+    pub(crate) neurons: Vec<Neuron>,
+    pub(crate) weights: Vec<Weight>,
 }
 
 impl Network {
@@ -56,26 +56,5 @@ impl Network {
     #[inline]
     pub fn weight(&self, idx: usize) -> &Weight {
         &self.weights[idx]
-    }
-
-    /// Returns a mutable reference to the `Layer` at the given index.
-    #[allow(dead_code)]
-    #[inline]
-    pub(crate) fn mut_layer(&mut self, idx: usize) -> &mut Layer {
-        &mut self.layers[idx]
-    }
-
-    /// Returns a mutable reference to the `Neuron` at the given index.
-    #[allow(dead_code)]
-    #[inline]
-    pub(crate) fn mut_neuron(&mut self, idx: usize) -> &mut Neuron {
-        &mut self.neurons[idx]
-    }
-
-    /// Returns a mutable reference to the `Weight` at the given index.
-    #[allow(dead_code)]
-    #[inline]
-    pub(crate) fn mut_weight(&mut self, idx: usize) -> &mut Weight {
-        &mut self.weights[idx]
     }
 }
--- src/network_builder.rs
+++ src/network_builder.rs
@@ -1,8 +1,7 @@
-use crate::prelude::Network;
+use crate::prelude::{Network, Layer};
 
 #[derive(Clone, Debug, Default, PartialEq, PartialOrd)]
 pub struct NetworkBuilder {
-    input_neurons: usize,
     layer_builders: Vec<LayerBuilder>,
 }
 
@@ -18,6 +17,7 @@ impl NetworkBuilder {
         Self::default()
     }
 
+    /// Adds a layer to the network.
     #[inline]
     pub fn add_layer(&self, layer_builder: LayerBuilder) -> Self {
         let mut new = self.clone();
@@ -26,6 +26,7 @@ impl NetworkBuilder {
         new
     }
 
+    /// Adds a layer to the network multiple times.
     #[inline]
     pub fn add_layers(&self, num: usize, layer_builder: LayerBuilder) -> Self {
         let mut new = self.clone();
@@ -36,20 +37,36 @@ impl NetworkBuilder {
         new
     }
 
+    /// Builds the final `Network`.
     #[inline]
     pub fn build(&self) -> Network {
-        println!("{:?}", self);
+        let mut network = Network::default();
 
-        Network::default()
+        let mut neurons = 0;
+        let mut weights = 0;
+        for l in 0..self.layer_builders.len() {
+            let neurons_in_layer = self.layer_builders[l].neurons;
+            network.layers.push(Layer { neurons: neurons_in_layer, neuron_start_idx: neurons_in_layer });
+
+            for n in 0..neurons_in_layer {
+                
+            }
+
+            neurons += neurons_in_layer;
+        }
+
+        network
     }
 }
 
 impl LayerBuilder {
+    /// Creates a new `LayerBuilder`.
     #[inline]
     pub(crate) fn new() -> Self {
         Self::default()
     }
 
+    /// Adds neurons to the layer.
     #[inline]
     pub fn add_neurons(&self, neurons: usize) -> Self {
         let mut new = self.clone();
