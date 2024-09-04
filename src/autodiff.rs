7805f719fff1825ce0b760724c738415b73c423f
Andrew <52798346+as042@users.noreply.github.com>
2024-09-03 23:13:38 -0400

    New Autodiff Progress

--- src/autodiff.rs
+++ src/autodiff.rs
@@ -1,13 +1,15 @@
-use std::{cell::RefCell, ops::{Add, Mul}};
+use std::{cell::RefCell, ops::{Add, Mul, Neg}};
 
 #[derive(Clone)]
 pub struct Grad {
     derivatives: Vec<f64>,
 }
 
-#[derive(Clone, Default)]
-pub struct Tape {
-    pub nodes: RefCell<Vec<Node>>,
+impl Grad {
+    #[inline]
+    pub fn wrt(&self, var: VarP) -> f64 {
+        self.derivatives[var.index]
+    }
 }
 
 #[derive(Clone, Copy)]
@@ -16,9 +18,21 @@ pub struct Node {
     pub parents: [usize; 2],
 }
 
+#[derive(Clone, Default)]
+pub struct Tape {
+    pub nodes: RefCell<Vec<Node>>,
+}
+
+#[derive(Clone, Copy)]
+pub struct VarP<'t> {
+    pub tape: &'t Tape,
+    pub index: usize,
+    pub val: f64,
+}
+
 impl Tape {
     #[inline]
-    pub fn var(&self, value: f64) -> Var {
+    pub fn new_var(&self, value: f64) -> VarP {
         let len = self.nodes.borrow().len();
         self.nodes.borrow_mut().push(
             Node {
@@ -27,7 +41,8 @@ impl Tape {
                 parents: [len, len],
             }
         );
-        Var {
+        
+        VarP {
             tape: self,
             index: len,
             val: value,
@@ -35,7 +50,7 @@ impl Tape {
     }
 
     #[inline]
-    pub fn binary_op(&self, lhs_partial: f64, rhs_partial: f64, lhs_index: usize, rhs_index: usize, new_value: f64) -> Var {
+    pub fn binary_op(&self, lhs_partial: f64, rhs_partial: f64, lhs_index: usize, rhs_index: usize, new_value: f64) -> VarP {
         let len = self.nodes.borrow().len();
         self.nodes.borrow_mut().push(
             Node {
@@ -44,7 +59,8 @@ impl Tape {
                 parents: [lhs_index, rhs_index],
             }
         );
-        Var {
+
+        VarP {
             tape: self,
             index: len,
             val: new_value,
@@ -52,14 +68,7 @@ impl Tape {
     }
 }
 
-#[derive(Clone, Copy)]
-pub struct Var<'t> {
-    pub tape: &'t Tape,
-    pub index: usize,
-    pub val: f64,
-}
-
-impl Var<'_> {
+impl VarP<'_> {
     /// Perform back propagation
     #[inline]
     pub fn backprop(&self) -> Grad {
@@ -87,25 +96,34 @@ impl Var<'_> {
     }
 }
 
-impl<'t> Add<Var<'t>> for Var<'t> {
+impl<'t> Add for VarP<'t> {
     type Output = Self;
 
     #[inline]
-    fn add(self, rhs: Var<'t>) -> Self::Output {
+    fn add(self, rhs: VarP<'t>) -> Self::Output {
         self.tape.binary_op(1.0, 1.0, self.index, rhs.index, self.val + rhs.val)
     }
 }
 
-impl<'t> Add<f64> for Var<'t> {
+impl<'t> Add<f64> for VarP<'t> {
     type Output = Self;
 
     #[inline]
     fn add(self, rhs: f64) -> Self::Output {
-        self.tape.binary_op(1.0, 0.0, self.index, rhs.index, self.val + rhs)
+        self.tape.binary_op(1.0, 0.0, self.index, self.index, self.val + rhs)
     }
 }
 
-impl<'t> Mul for Var<'t> {
+impl<'t> Add<VarP<'t>> for f64 {
+    type Output = VarP<'t>;
+
+    #[inline]
+    fn add(self, rhs: VarP<'t>) -> VarP<'t> {
+        rhs + self
+    }
+}
+
+impl<'t> Mul for VarP<'t> {
     type Output = Self;
 
     #[inline]
@@ -114,13 +132,43 @@ impl<'t> Mul for Var<'t> {
     }
 }
 
+impl<'t> Mul<f64> for VarP<'t> {
+    type Output = Self;
+
+    #[inline]
+    fn mul(self, rhs: f64) -> Self::Output {
+        self.tape.binary_op(rhs, 0.0, self.index, self.index, self.val * rhs)
+    }
+}
+
+impl<'t> Mul<VarP<'t>> for f64 {
+    type Output = VarP<'t>;
+
+    #[inline]
+    fn mul(self, rhs: VarP<'t>) -> VarP<'t> {
+        rhs * self
+    }
+}
+
+impl<'a> Neg for VarP<'a> {
+    type Output = Self;
+
+    #[inline]
+    fn neg(self) -> Self::Output {
+        self * -1.
+    }
+}
+
 #[test]
 fn test() {
     let tape = Tape::default();
-    let x = tape.var(1.0);
-    let y = tape.var(1.0);
+    let x = tape.new_var(1.0);
+    let y = tape.new_var(1.0);
     let z = -2.0 * x + x * x * x * y + 2.0 * y;
     let grad = z.backprop();
+
+    println!("full grad: {:?}", grad.derivatives);
     println!("dz/dx of z = -2x + x^3 * y + 2y at x=1.0, y=1.0 is {}", grad.wrt(x));
     println!("dz/dy of z = -2x + x^3 * y + 2y at x=1.0, y=1.0 is {}", grad.wrt(y));
+    panic!()
 }
